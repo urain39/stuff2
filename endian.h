b2556709cdc9912293f838ff064081ba42010c49
urain39 <16981964+urain39@users.noreply.github.com>
2020-05-10 03:38:04 +0800

    Update endian.h

--- endian.h
+++ endian.h
@@ -1,13 +1,13 @@
 #define SWAP_BYTES(n) ( \
-	((n & 0xff) << 8) | ((n & 0xff00) >> 8) \
+	(n << 8) | (n >> 8) \
 )
 
 #define SWAP_BYTES32(n) ( \
-	(SWAP_BYTES(n & 0xffff) << 16) | (SWAP_BYTES((n & 0xffff0000) >> 16)) \
+	(SWAP_BYTES(n << 16)) | (SWAP_BYTES(n >> 16)) \
 )
 
 #define SWAP_BYTES64(n) ( \
-	(SWAP_BYTES32(n & 0xffffffff) << 32) | (SWAP_BYTES32((n & 0xffffffff00000000) >> 32)) \
+	(SWAP_BYTES32(n << 32)) | (SWAP_BYTES32(n >> 32)) \
 )
 
 /**
