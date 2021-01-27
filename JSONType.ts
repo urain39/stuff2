5ae706be46503a4dc16379799c9ae17423371b68
urain39 <16981964+urain39@users.noreply.github.com>
2021-01-27 22:14:46 +0800

    Update JSONType.ts

--- JSONType.ts
+++ JSONType.ts
@@ -1,6 +1,12 @@
-export interface IMap<V> {
+interface IMap<V> {
     [key: string]: V | V[];
     [index: number]: V | V[];
 }
 
-export interface JSONType extends IMap<JSONType | string | number | null> {};
+type JSONPrimitiveType = number | string | boolean | null;
+
+interface JSONObjectType extends IMap<JSONPrimitiveType | JSONObjectType> {};
+
+interface JSONArrayType extends Array<JSONPrimitiveType | JSONObjectType | JSONArrayType> {};
+
+export type JSONType = JSONPrimitiveType | JSONObjectType | JSONArrayType;
