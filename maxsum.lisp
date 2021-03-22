6e19a0bde7033ac26db4d8fb6619fbf5a1d783bd
urain39 <16981964+urain39@users.noreply.github.com>
2021-03-22 00:32:31 -0500

    Update maxsum.lisp

--- maxsum.lisp
+++ maxsum.lisp
@@ -1,19 +1,16 @@
-(defun my-max (x y)
-	(if (> x y) x y))
+(defun my-min (x y)
+	(if (< x y) x y))
 
-(defun max-one (x y z)
-	(my-max (my-max x y) z))
+(defun min-one (x y z)
+	(my-min (my-min x y) z))
 
-(defun max-second (x y z)
-	(let ((max (max-one x y z)))
+(defun max-sum (x y z)
+	(let ((min_ (min-one x y z)))
 		(cond
-			((= x max) (my-max y z))
-			((= y max) (my-max x z))
-			((= z max) (my-max x y))
+			((= x min_) (+ y z))
+			((= y min_) (+ x z))
+			((= z min_) (+ x y))
 		)
 	))
 
-(defun my-sum (x y z)
-	(+ (max-one x y z) (max-second x y z)))
-
-(my-sum 1 6 3)
+(max-sum 1 6 3)
