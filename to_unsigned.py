997a421ffce5f4c835523097cb8f541d46c78112
urain39 <16981964+urain39@users.noreply.github.com>
2021-04-14 02:00:40 -0500

    Create to_unsigned.py

---
+++ to_unsigned.py
@@ -0,0 +1,13 @@
+def to_unsigned(number, bitwidth=32):
+	assert bitwidth > 1
+	max_value = (1 << bitwidth - 1) - 1
+	if number < 0:
+		number = -number
+		assert number <= max_value + 1
+		number = (~number & ((1 << bitwidth) - 1)) + 1
+	else:
+		assert number <= max_value
+	return number
+
+# (unsigned char)-128
+to_unsigned(-1, 8)
