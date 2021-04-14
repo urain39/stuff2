e5baf3d27de267ed5631ae10f61bdd6d07e59d12
urain39 <16981964+urain39@users.noreply.github.com>
2021-04-14 03:52:44 -0500

    Create to_signed.py

--- to_signed.py
+++ to_signed.py
@@ -1,12 +1,10 @@
 def to_signed(number, bitwidth):
-	# Should we handle negative value?
-	# number = to_unsigned(number, bitwidth)
 	assert number >= 0
 	assert bitwidth > 1
 	max_value = (1 << bitwidth) - 1
 	assert number <= max_value
 	if number & (1 << bitwidth - 1):
-		number = -(~(number - 1) & ((1 << bitwidth) - 1))
+		number = -(~(number - 1) & max_value)
 	return number
 
 # (short)65535
