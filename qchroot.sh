5c657959342677ab8f0d03005a5873d60e15638d
μrainЗ⑨ <16981964+urain39@users.noreply.github.com>
2021-05-15 01:17:09 -0500

    Create qchroot.sh

---
+++ qchroot.sh
@@ -0,0 +1,59 @@
+#!/system/bin/sh
+
+
+if [ -z "$QCHROOT_ARCH" ]; then
+	QCHROOT_ARCH="$(uname -m)"
+fi
+
+
+show_usage() {
+	echo "Usage: qchroot [OPTIONS] <NEWROOT> [PROGRAM [ARGS]]"
+	echo ""
+	echo "Options:"
+	echo "    -a, --arch    set qemu arch manually. (default: $QCHROOT_ARCH)"
+}
+
+
+main() {
+	while :; do
+		case "$1" in
+			"-a")
+				shift
+				QCHROOT_ARCH="$1"
+				;;
+			"--arch"*)
+				QCHROOT_ARCH="${1:7}"
+				;;
+			*)
+				break
+				;;
+			esac
+			shift
+	done
+
+	if [ $# -lt 1 ]; then
+		show_usage
+		exit 1
+	fi
+
+	QCHROOT_NEWROOT="$1"
+	shift
+
+	if [ $# -lt 1 ]; then
+		if [ -x "$QCHROOT_NEWROOT/bin/su" ]; then
+			QCHROOT_PROGRAM="/bin/su -"
+		else
+			QCHROOT_PROGRAM="/bin/sh -"
+		fi
+	else
+		QCHROOT_PROGRAM="$@"
+	fi
+
+	proot \
+		-q "qemu-$QCHROOT_ARCH" \
+		-b /system:/system \
+		-S "$QCHROOT_NEWROOT" \
+		$QCHROOT_PROGRAM
+}
+
+main "$@"
