32b235dd200fc871c744115f1081622bf9fee47a
μrainЗ⑨ <16981964+urain39@users.noreply.github.com>
2021-09-27 09:02:05 +0800

    Create setup-crontabs.sh

---
+++ setup-crontabs.sh
@@ -0,0 +1,26 @@
+if [ "$USER" = "root" ]; then
+    echo "Please run it as non-root user!" >&2
+    exit 1
+fi
+
+# Set default permission
+umask 022
+
+# Create necessary directory
+mkdir -p ~/.local/cron/tabs
+mkdir -p ~/.local/cron/00closely
+mkdir -p ~/.local/cron/01hourly
+mkdir -p ~/.local/cron/02daily
+mkdir -p ~/.local/cron/03weekly
+mkdir -p ~/.local/cron/04monthly
+
+# Add default crontab
+crontab - << EOF
+# m h       dom mon   dow   cmd
+  */15 *    * *       *     run-parts ~/.local/cron/00closely
+  10 *      * *       *     run-parts ~/.local/cron/01hourly
+  20 22     * *       *     run-parts ~/.local/cron/02daily
+# NOTE: Busybox cron expression use 0 as sunday only
+  30 22     * *       0     run-parts ~/.local/cron/03weekly
+  40 22     1 *       *     run-parts ~/.local/cron/04monthly
+EOF
