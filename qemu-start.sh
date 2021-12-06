bd69dd7f7e268aab126d40cd58678afb9193f58e
urain39 <urain39@qq.com>
2021-12-06 21:21:02 +0800

    qemu-start.sh: forward samba port

--- qemu-start.sh
+++ qemu-start.sh
@@ -13,7 +13,9 @@
 hostfwd=tcp::20021-:21,\
 hostfwd=tcp::20022-:22,\
 hostfwd=tcp::20080-:80,\
+hostfwd=tcp::20139-:139,\
 hostfwd=tcp::20443-:443,\
+hostfwd=tcp::20445-:445,\
 hostfwd=tcp::26800-:6800,\
 hostfwd=tcp::28080-:8080,\
 hostfwd=tcp::28384-:8384,\
