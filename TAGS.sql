7b8d733cece48dfaa5df2e2e9fe1a1324f2a5845
urain39 <urain39@qq.com>
2022-09-02 23:02:56 +0800

    update TAGS.sql

--- TAGS.sql
+++ TAGS.sql
@@ -3,71 +3,93 @@
 --        --------------------
 --
 --  原理主要是将以 \x00 分隔的字符串当数组使用
---  然后再使用 instr 搜索被 \x00 包裹的字符串
+--  然后再使用 INSTR 搜索被 \x00 包裹的字符串
 --  理论上 \x00 换成逗号也行（记得改开头和结尾）
---  此外下面的字符串也可以直接换成字节（整数类型）
+--
+--  此外也可以直接将 tag 编码成 Unicode 字符
+--  配合 CHECK (tag_id > 0) 可以实现快速查询
 ------------------------------------------
+
+-- CREATE TABLE tags (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT CHECK (id > 0),
+--   name TEXT NOT NULL,
+--   time TEXT NOT NULL
+-- );
+
+-- 假如某个 file 的 tags 存在 tag_id > 0 那么可以使用下面的语句修复
+-- UPDATE files SET tags=REPLACE(tags, "\x00\x00\x00", "")
+--   WHERE INSTR(tags, "\x00\x00\x00");
 
 CREATE TABLE files (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name nvarchar[64] NOT NULL,
-  tags nvarchar[64] NOT NULL
+  name TEXT NOT NULL,
+  size INTEGER NOT NULL,
+  tags TEXT NOT NULL
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - きらいな人.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - しんでしまうとはなさけない!.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - 夕立のりぼん.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - 路地裏猫の正体.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - After This.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - ギガンティックO.T.N.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - 威風堂々.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - シﾞュリエッタとロミヲ.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 INSERT INTO files VALUES (
   NULL,
   "Au - Glow.mp3",
+  0,
   "\x00Au\x00ぺろ。\x00"
 );
 
 SELECT id FROM files
-  WHERE instr(tags, "\x00Au\x00")
-    AND instr(tags, "\x00ぺろ。\x00");
+  WHERE INSTR(tags, "\x00Au\x00")
+    AND INSTR(tags, "\x00ぺろ。\x00");
