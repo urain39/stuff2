085c68ab900a5aa4050b73c3051febc8af936c2a
urain39 <urain39@qq.com>
2022-09-13 03:08:57 +0800

    update to_{,un}singed.py

--- to_signed.py
+++ to_signed.py
@@ -1,11 +1,15 @@
-def to_signed(number, bitwidth):
+def to_signed(number, bitwidth=32):
 	assert number >= 0
 	assert bitwidth > 1
-	max_value = (1 << bitwidth) - 1
-	assert number <= max_value
-	if number & (1 << bitwidth - 1):
-		number = -(~(number - 1) & max_value)
-	return number
+	# 1. 在忽略高位的情况下将负数还原
+	# 2. 使用掩码将高位去掉以得到原值
+	# 3. 最后再将原值转换为正确的数值
+	#
+	# 注：你可能好奇高位的 1 去哪了？它
+	# 实际上在第一次翻转时就被转换成 0 了
+	return -(-number & ((1 << bitwidth) - 1)) \
+	  if number & (1 << bitwidth - 1) \
+	  else number & ((1 << bitwidth) - 1)
 
 # (short)65535
-to_signed(65535, 16)
+print(to_signed(65535, 16))
--- to_unsigned.py
+++ to_unsigned.py
@@ -1,13 +1,6 @@
 def to_unsigned(number, bitwidth=32):
 	assert bitwidth > 1
-	max_value = (1 << bitwidth - 1) - 1
-	if number < 0:
-		number = -number
-		assert number <= max_value + 1
-		number = (~number & ((1 << bitwidth) - 1)) + 1
-	else:
-		assert number <= max_value
-	return number
+	return number & ((1 << bitwidth) - 1)
 
 # (unsigned char)-1
-to_unsigned(-1, 8)
+print(to_unsigned(-1, 8))
