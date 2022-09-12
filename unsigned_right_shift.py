2b5e81b0ef93c64462eee90d4c12b866623e3bdc
urain39 <urain39@qq.com>
2022-09-13 06:38:03 +0800

    update unsigned_right_shift.py

--- unsigned_right_shift.py
+++ unsigned_right_shift.py
@@ -1,13 +1,8 @@
 def unsigned_right_shift(number, offset, bitwidth=32):
 	assert bitwidth > 1
-	max_value = (1 << bitwidth - 1) - 1
-	if number < 0:
-		number = -number
-		assert number <= max_value + 1
-		number = (~number & ((1 << bitwidth) - 1)) + 1
-	else:
-		assert number <= max_value
-	return number >> offset
+	return (number & ((1 << bitwidth) - 1) \
+	  if number < 0 \
+	  else number & ((1 << bitwidth) - 1)) >> offset
 
 # (char)-128 >>> 2
 unsigned_right_shift(-128, 2, 8)
