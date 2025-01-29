5b3e90d0572d11e990e500a9981af27a065f7a9d
urain39 <urain39@qq.com>
2025-01-29 15:26:59 +0800

    add vidcpr.sh

---
+++ vidcpr.sh
@@ -0,0 +1,12 @@
+for v in *.mp4; do
+  case "$v" in
+    !*)
+      continue
+      ;;
+  esac
+  [ -f "!$v" ] && continue
+  ffmpeg -i "$v" \
+    -c:v hevc -preset:v veryfast -g 240 -bf 4 -refs 3 -qcomp 0.75 -qmin 22 -qmax 33 \
+    -c:a aac -q:a 1 \
+    "!$v"
+done
