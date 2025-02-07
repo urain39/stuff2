fbb7d323eabf1e6d784c389c4c72f0e2759985f0
urain39 <urain39@qq.com>
2025-02-07 18:57:17 +0800

    vidcpr.sh: optimize dark scenes

--- vidcpr.sh
+++ vidcpr.sh
@@ -6,7 +6,7 @@ for v in *.mp4; do
   esac
   [ -f "!$v" ] && continue
   ffmpeg -i "$v" \
-    -c:v hevc -preset:v veryfast -g 240 -bf 4 -refs 3 -qcomp 0.75 -qmin 22 -qmax 33 \
+    -c:v hevc -preset:v veryfast -g 240 -bf 4 -refs 3 -qcomp 0.75 -qmin 22 -qmax 33 -x265-params aq-mode=3 \
     -c:a aac -q:a 1 \
     "!$v"
 done
