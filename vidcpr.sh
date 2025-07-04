2ac08fb6e765e6b3ddd134500ef383baa2374d67
urain39 <urain39@qq.com>
2025-07-04 10:30:56 +0800

    update vidcpr.sh

--- vidcpr.sh
+++ vidcpr.sh
@@ -6,7 +6,8 @@ for v in *.mp4; do
   esac
   [ -f "!$v" ] && continue
   ffmpeg -i "$v" \
-    -c:v hevc -preset:v fast -g 240 -bf 4 -refs 3 -qcomp 0.75 -qmin 22 -qmax 29 -x265-params aq-mode=3 \
+    -c:v hevc -preset:v fast -g 240 -bf 8 -refs 5 -qcomp 0.75 -qmin 30 -qmax 39 \
+    -x265-params aq-mode=3:aq-strength=1.25:deblock=-1,-1:me=star:merange=64:no-amp=1:no-sao=1:psy-rd=2.2:psy-rdoq=1.33:subme=6:weightb=1 \
     -c:a aac -q:a 1 \
     "!$v"
 done
