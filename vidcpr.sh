a685c95af046913f787886636bb015b8195a3def
urain39 <urain39@qq.com>
2026-07-14 02:11:21 +0800

    vidcpr.sh: VMAF -> MS-SSIM, update code

--- vidcpr.sh
+++ vidcpr.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+CRF="${1:-"27"}"
+
 for v in *.mp4; do
-  case "${v}" in
+  case "${V}" in
     !*)
       continue
       ;;
     *)
       ;;
   esac
-  [ -f "!${v}" ] && continue
-  ffmpeg -i "${v}" \
-    -c:v libsvtav1 -preset 6 -g 120 -bf 8 -refs 5 -crf 27 -pix_fmt yuv420p10le \
-    -svtav1-params scd=1:superres-mode=3:superres-qthres=22:tune=5 \
+  [ -f "!${V}" ] && continue
+  ffmpeg -i "${V}" \
+    -c:v libsvtav1 -preset 6 -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
+    -svtav1-params rc=0:scd=1:superres-mode=3:superres-qthres=22:tune=4 \
     -c:a aac -ac 2 -q:a 1 \
-    "!${v}"
+    "!${V}"
 done
