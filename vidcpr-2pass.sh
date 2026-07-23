e8f06479cb3ee8cfc43da6e0bec8d3f7190d7386
urain39 <urain39@qq.com>
2026-07-24 04:38:26 +0800

    vidcpr*.sh: preset 6 -> preset 5

--- vidcpr-2pass.sh
+++ vidcpr-2pass.sh
@@ -12,13 +12,13 @@ for V in *.mp4; do
   esac
   [ -f "!${V}" ] && continue
   ffmpeg -i "${V}" \
-    -c:v libsvtav1 -preset 6 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
+    -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
     -svtav1-params rc=1:scd=1:tune=0 -pass 1 \
     -an \
     -f null \
     "/dev/null"
   ffmpeg -i "${V}" \
-    -c:v libsvtav1 -preset 6 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
+    -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
     -svtav1-params rc=1:scd=1:tune=0 -pass 2 \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
--- vidcpr.sh
+++ vidcpr.sh
@@ -12,7 +12,7 @@ for V in *.mp4; do
   esac
   [ -f "!${V}" ] && continue
   ffmpeg -i "${V}" \
-    -c:v libsvtav1 -preset 6 -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
+    -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
     -svtav1-params rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune=4 \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
