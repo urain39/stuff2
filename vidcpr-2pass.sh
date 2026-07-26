00db727db070f438fbcbb9c3273a104db84902b3
urain39 <urain39@qq.com>
2026-07-26 09:40:52 +0800

    vidcpr*.sh: enable strong deblocking loop filter

--- vidcpr-2pass.sh
+++ vidcpr-2pass.sh
@@ -13,13 +13,13 @@ for V in *.mp4; do
   [ -f "!${V}" ] && continue
   ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
-    -svtav1-params rc=1:scd=1:tune=0 -pass 1 \
+    -svtav1-params enable-dlf=2:rc=1:scd=1:tune=0 -pass 1 \
     -an \
     -f null \
     "/dev/null"
   ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
-    -svtav1-params rc=1:scd=1:tune=0 -pass 2 \
+    -svtav1-params enable-dlf=2:rc=1:scd=1:tune=0 -pass 2 \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
 done
