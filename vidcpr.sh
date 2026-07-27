2d2d5e052d724c8482844cb86feb44ce6e989fda
urain39 <urain39@qq.com>
2026-07-27 18:03:07 +0800

    vidcpr.sh: 修改输入参数顺序；可更改预设级别

--- vidcpr.sh
+++ vidcpr.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 CRF="${1:-"30"}"
-TUN="${2:-"0"}"
+PRE="${2:-"6"}"
+TUN="${3:-"0"}"
 
 for V in *.mp4; do
   case "${V}" in
@@ -14,7 +15,7 @@ for V in *.mp4; do
   [ -f "!${V}" ] && continue
   # shellcheck disable=SC2140
   ffmpeg -i "${V}" \
-    -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
+    -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
     -svtav1-params enable-dlf=2:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
