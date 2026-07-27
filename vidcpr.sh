4125d51a4506999e3380d65d110244235f66c5f4
urain39 <urain39@qq.com>
2026-07-27 14:10:53 +0800

    vidcpr.sh: pass $2 to --tune

--- vidcpr.sh
+++ vidcpr.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-CRF="${1:-"27"}"
+CRF="${1:-"30"}"
+TUN="${2:-"0"}"
 
 for V in *.mp4; do
   case "${V}" in
@@ -11,9 +12,10 @@ for V in *.mp4; do
       ;;
   esac
   [ -f "!${V}" ] && continue
+  # shellcheck disable=SC2140
   ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset 5 -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
-    -svtav1-params enable-dlf=2:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune=4 \
+    -svtav1-params enable-dlf=2:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
 done
