9d4ebc7ba15f068dfc691e1116a0cb9f6b818e7d
urain39 <urain39@qq.com>
2026-08-04 16:23:12 +0800

    vidcpr*.sh: improve video quality

--- vidcpr-2pass.sh
+++ vidcpr-2pass.sh
@@ -13,15 +13,15 @@ for V in *.mp4; do
       ;;
   esac
   [ -f "!${V}" ] && continue
-  ffmpeg -i "${V}" \
+  taskset -a f0 ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
-    -svtav1-params enable-dlf=2:rc=1:scd=1:tune="${TUN}" -pass 1 \
+    -svtav1-params enable-dlf=2:enable-variance-boost=1:lp=4:rc=1:scd=1:tune="${TUN}" -pass 1 \
     -an \
     -f null \
     "/dev/null"
-  ffmpeg -i "${V}" \
+  taskset -a f0 ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
-    -svtav1-params enable-dlf=2:rc=1:scd=1:tune="${TUN}" -pass 2 \
+    -svtav1-params enable-dlf=2:enable-variance-boost=1:lp=4:rc=1:scd=1:tune="${TUN}" -pass 2 \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
 done
--- vidcpr.sh
+++ vidcpr.sh
@@ -14,9 +14,9 @@ for V in *.mp4; do
   esac
   [ -f "!${V}" ] && continue
   # shellcheck disable=SC2140
-  ffmpeg -i "${V}" \
+  taskset -a f0 ffmpeg -i "${V}" \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
-    -svtav1-params enable-dlf=2:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
+    -svtav1-params enable-dlf=2:enable-variance-boost=1:lp=4:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
     -c:a aac -ac 2 -q:a 1 \
     "!${V}"
 done
