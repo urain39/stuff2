2d524464af7e7f2bd626650714dc8e202f8d1dda
urain39 <urain39@qq.com>
2026-08-06 21:45:27 +0800

    vidcpr*.sh: 1080p -> min(i[wh],1080)p

--- vidcpr.sh
+++ vidcpr.sh
@@ -15,7 +15,7 @@ for V in *.mp4; do
   [ -f "!${V}" ] && continue
   # shellcheck disable=SC2140
   taskset -a f0 ffmpeg -i "${V}" \
-    -vf 'scale=if(lte(iw\,ih)\,1080\,-1):if(lte(iw\,ih)\,-1\,1080),crop=iw-mod(iw\,8):ih-mod(ih\,8)' \
+    -vf 'scale=if(lte(iw\,ih)\,min(iw\,1080)\,-1):if(lte(iw\,ih)\,-1\,min(ih\,1080)),crop=iw-mod(iw\,8):ih-mod(ih\,8)' \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
     -svtav1-params ac-bias=1.0:enable-dlf=2:enable-variance-boost=1:lp=4:rc=0:scd=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
     -c:a libopus -ac 2 -b:a 76.8K \
