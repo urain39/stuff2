41090a6e23e9e9ba7044fc6005586f8d36a41cbb
urain39 <urain39@qq.com>
2026-08-06 18:09:23 +0800

    vidcpr*.sh: fix hwaccel for some SoCs

--- vidcpr-2pass.sh
+++ vidcpr-2pass.sh
@@ -14,14 +14,16 @@ for V in *.mp4; do
   esac
   [ -f "!${V}" ] && continue
   taskset -a f0 ffmpeg -i "${V}" \
+    -vf 'scale=if(lte(iw\,ih)\,1080\,-1):if(lte(iw\,ih)\,-1\,1080),crop=iw-mod(iw\,8):ih-mod(ih\,8)' \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
     -svtav1-params ac-bias=1.0:enable-dlf=2:enable-variance-boost=1:lp=4:rc=1:scd=1:tune="${TUN}" -pass 1 \
     -an \
     -f null \
     "/dev/null"
   taskset -a f0 ffmpeg -i "${V}" \
+    -vf 'scale=if(lte(iw\,ih)\,1080\,-1):if(lte(iw\,ih)\,-1\,1080),crop=iw-mod(iw\,8):ih-mod(ih\,8)' \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -b:v "${TBR}" -pix_fmt yuv420p10le \
     -svtav1-params ac-bias=1.0:enable-dlf=2:enable-variance-boost=1:lp=4:rc=1:scd=1:tune="${TUN}" -pass 2 \
-    -c:a libopus -ac 2 -b:a 64K \
+    -c:a libopus -ac 2 -b:a 76.8K \
     "!${V}"
 done
