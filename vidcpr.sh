20bfb12e89eaf9ad92bb079213366545e70c29df
urain39 <urain39@qq.com>
2026-08-09 00:54:46 +0800

    Revert "vidcpr*.sh: 映射多声道到双声道。"
    This reverts commit 120cbef9a0bd3aac38e719a6857e49140d63b51d.

--- vidcpr.sh
+++ vidcpr.sh
@@ -16,7 +16,6 @@ for V in *.mp4; do
   # shellcheck disable=SC2140
   taskset -a f0 ffmpeg -i "${V}" \
     -vf 'scale=if(lte(iw\,ih)\,min(iw\,1080)\,-1):if(lte(iw\,ih)\,-1\,min(ih\,1080)),crop=iw-mod(iw\,8):ih-mod(ih\,8)' \
-    -af 'sofalizer=sofa='"${PREFIX-:"/usr"}"'/share/libmysofa/default.sofa:type=time' \
     -c:v libsvtav1 -preset "${PRE}" -g 120 -bf 8 -refs 5 -crf "${CRF}" -pix_fmt yuv420p10le \
     -svtav1-params ac-bias=1.0:enable-cdef=1:enable-dlf=2:enable-kf-tf=1:enable-restoration=1:enable-tf=1:enable-variance-boost=1:lp=4:qp-scale-compress-strength=1:rc=0:scd=1:scm=3:sharpness=1:superres-mode=3:superres-qthres="$((CRF - 5))":tune="${TUN}" \
     -c:a libopus -ac 2 -b:a 76.8K \
